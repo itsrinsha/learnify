356cff0bb8a2187fb3d5e379a627e620e39784f0
itsrinsha <rinshaa279@gmail.com>
2026-05-09 22:12:07 +0530

    Update Learnify project structure and features

--- frontend/src/services/liveService.js
+++ frontend/src/services/liveService.js
@@ -3,6 +3,7 @@ import axiosInstance from '../features/axiosInstance';
 export const getMyLiveSessions = async () => {
   try {
     const response = await axiosInstance.get('/users/my-live-sessions');
+    console.log(response.data)
     return response.data;
   } catch (error) {
     console.error('Error fetching my live sessions:', error);
